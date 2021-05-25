cb85eb33b7209e8969cc4868c7ef4c685594f27d
bgamas <bryangaso15@gmail.com>
2021-05-24 23:19:18 -0500

    feat: initial commit

---
+++ components/Hero.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hero = () => {
+  return <div className="Hero">HERO!</div>;
+};
+
+export default Hero;
---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+import Hero from "@components/Hero";
+import React from "react";
+
+const Home = () => {
+  return <Hero></Hero>;
+};
+
+export default Home;
