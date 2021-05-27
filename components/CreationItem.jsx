784ae7ab193325b912001d3f22f53639c272c0f6
bgamas <bryangaso15@gmail.com>
2021-05-26 22:49:46 -0500

    refactor: + CreationItem component

---
+++ components/CreationItem.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { creationListDesktop } from "utils/creationList";
+
+const CreationItem = ({ mobile, index }) => {
+  const desktop = creationListDesktop[index];
+  return (
+    <figure>
+      <picture>
+        <source media="(min-width:1000px)" srcSet={desktop.src} />
+        <img src={mobile.src} alt={mobile.alt} />
+        <div className="image-shadow"></div>
+      </picture>
+      <figurecaption>{mobile.alt}</figurecaption>
+      <style jsx>
+        {`
+          figure {
+            position: relative;
+          }
+
+          .image-shadow {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            box-shadow: inset 100px 0px 60px rgba(0, 0, 0, 0.5);
+            z-index: 1;
+          }
+
+          figurecaption {
+            position: absolute;
+            left: 2.2rem;
+            bottom: 2.5rem;
+            width: 40%;
+            font-family: "Josefin Sans", sans-serif;
+            font-size: 2.5rem;
+            text-align: left;
+            color: var(--white);
+            z-index: 2;
+          }
+        `}
+      </style>
+    </figure>
+  );
+};
+
+export default CreationItem;
